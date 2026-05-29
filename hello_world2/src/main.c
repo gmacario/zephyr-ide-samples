84e8f09ef44cd23848ddcfad1d9d838642b1e568
Gianpaolo Macario <gianpaolo.macario@arol.com>
2026-05-29 11:39:57 +0200

    feat(hello_world2): add countdown from 1000 to 0

--- hello_world2/src/main.c
+++ hello_world2/src/main.c
@@ -5,10 +5,20 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	int count = 1000;
+	const k_timeout_t delay_ms = K_MSEC(1000);		// 1 second
+
+	while (count > 0) {
+		printf("DEBUG: count=%d\n", count);
+		k_sleep(delay_ms);
+		count--;
+	}
 
 	return 0;
 }
